b92acc4a372c771e461a7140b4ffaff5dbc80d21
Meghan Denny <hello@nektro.net>
2026-06-07 20:47:18 -0700

    update to zig 0.16.0

--- test.zig
+++ test.zig
@@ -1,7 +1,13 @@
 const std = @import("std");
 const icu = @import("icu");
 
-// TODO: run the official test suite from unicode
+// TODO: run the official tests from unicode
 test {
-    std.testing.refAllDeclsRecursive(icu);
+    _ = &icu.hasDerivedCoreProperty;
+    _ = &icu.hasProperty;
+    _ = &icu.hasEmojiCategory;
+    _ = &icu.age;
+    _ = &icu.eastAsianWidth;
+    _ = &icu.bidiPairedBracketType;
+    _ = &icu.toLower;
 }
